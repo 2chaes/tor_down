a20a38fa598d57a27a3567cb48c007ba0850a45e
ChaeSeung Lee <2chaes@gmail.com>
2017-03-04 20:10:45 +0900

    Option : script-torrent-done-filename

---
+++ done_tor.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# 'transmission 주소:port -n id:passwd' 형식으로 작성
+SERVER=''
+transmission-remote $SERVER -t $TR_TORRENT_ID -r
+
+
+# 사용하실분은 주석 제거후 사용하세요
+#BUPUSH='' # 'PushBullet 키값'
+#YMD=`date +%y%m%d`
+#TIME=`date +%H:%M:%S`
+#curl -u "$BUPUSH": https://api.pushbullet.com/v2/pushes -d type=note -d title="완료 : $TR_TORRENT_NAME" -d body="$YMD | $TIME | $TR_TORRENT_NAME 다운로드 완료" --insecure
